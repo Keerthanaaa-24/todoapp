7a89074a38e61f2dae66500dbc0f1ea6b98004ff
Keerthu <keerthanamr1307@gmail.com>
2026-04-10 20:17:50 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+
 const taskRoutes = require("./routes/tasks");
 const authRoutes = require("./routes/auth");
 
